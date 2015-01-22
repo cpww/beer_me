4bf670e78134509b78fba032e8d7620b9189ae45
shiggiddie <sean.dennison.osu@gmail.com>
2015-01-21 18:57:29 -0600

    remove jade, get index working (error needs fixed, though

--- app/app.js
+++ app/app.js
@@ -10,10 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
@@ -39,10 +35,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+        res.render('error');
     });
 }
 
@@ -50,10 +43,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    res.render('error');
 });
 
 
