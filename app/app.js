a1b223a9999c79c635ae622ef47f49dc04f111f8
shiggiddie <sean.dennison.osu@gmail.com>
2015-01-27 19:30:06 -0600

    create /api/v1/beers endpoint

--- app/app.js
+++ app/app.js
@@ -4,8 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var routes = require('./routes/index');
+var secrets = require('./secrets');
 var users = require('./routes/users');
 
 var app = express();
@@ -20,11 +22,45 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+//app.use(express.bodyParser()); need?
 
 app.get('/', function(req, res, next) {
     console.log('routing to index');
     res.render('index');
     });
+
+app.post('/api/v1/beers', function(req, res, next) {
+    // Construct the brewerydb endpoint to hit
+    endpoint = 'http://api.brewerydb.com/v2/search?q=' + encodeURIComponent(req.body.beer) + '&key=' + secrets.bdb_key
+    console.log(endpoint);
+
+    // Hit the endpoint
+    request(endpoint, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var data = JSON.parse(body).data;
+            var matches = 0
+
+            // Create an array that filters the return data such that the last
+            // entry in the array will be the data entry that had the most words
+            // that matched the original queried beer
+            var qualifiers = data.filter( function(el) {
+                var cur_matches = el.name.split(' ').filter( function(word) {
+                    return req.body.beer.indexOf(word) > -1;
+                }).length;
+                if (matches <= cur_matches) {
+                    matches = cur_matches;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            });
+
+            // Return a JSON response of the last entry in the qualifiers array
+            res.send(JSON.stringify(qualifiers[qualifiers.length-1]));
+        }
+    })
+});
 
 // catch 404 and forward to error handler
 // This is due to the way express attemps to match a requested
