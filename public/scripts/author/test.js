590f3b77b5538055c7466b87b50936e36bb630fa
DLavin23 <daniel.e.lavin@gmail.com>
2015-02-04 20:33:38 -0600

    WIP stash

--- public/scripts/author/test.js
+++ public/scripts/author/test.js
@@ -1,1 +1,22 @@
-console.log('loaded');
+angular.module('app', [function() {
+	console.log('hello, world!');
+}])
+
+.controller('AppCtrl', ['$scope', function($scope, $http){
+  console.log('App controller');
+
+	var app = this;
+
+  app.searchBeers = function(beer) {
+  	console.log('my beer:' + beer);
+  	$http.post("http://localhost:3000/api/v1/beers", beer)
+  		.success(function(data) {
+  			console.log(data);
+  		})
+  }
+
+  // $scope.addBeer = function(beer) {
+  // 	console.log('my beer:' + beer);
+  // }
+}]);
+
