38b824938b29db030f5d632b5bee7ceed59089b9
Anthony D'Andrea <anthony.dandrea@me.com>
2015-02-18 20:32:42 -0600

    update secretz

--- app/secrets_example.js
+++ app/secrets_example.js
@@ -1,3 +1,5 @@
-var secrets = {'bdbKey':'INSERT YOUR BREWERYDB API KEY HERE'}
+var secrets = {'bdbKey':'INSERT YOUR BREWERYDB API KEY HERE',
+							'utdIdKey':'INSERT YOUR UNTAPPD CLIENT ID API KEY HERE',
+							'utdSecretKey':'INSERT YOUR UNTAPPD SECRET ID API KEY HERE'};
 
 module.exports = secrets;
